9ac6dbdd4a9a48448a34cfe4385e1acd76e61075
kernelpanic007 <josalamanca007@gmail.com>
2024-02-16 14:03:58 -0600

    Update zabbixBC250.sh

--- zabbixBC250.sh
+++ zabbixBC250.sh
@@ -23,25 +23,24 @@ screen -S wart_miner -X quit
 
 #install zabbix agent
 cd /home/user
-wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1%2Bubuntu18.04_all.deb
-dpkg -i zabbix-release_6.4-1+ubuntu18.04_all.deb 
+wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1%2Bubuntu20.04_all.deb
+dpkg -i zabbix-release_6.4-1+ubuntu20.04_all.deb
 
 apt update 
 apt -y install zabbix-agent 
 
 
 #download necessary scripts
-wget https://raw.githubusercontent.com/kernelpanic0007/hiveOSScripts/main/amd-oc.conf -O /hive-config/amd-oc.conf
-wget https://raw.githubusercontent.com/kernelpanic0007/hiveOSScripts/main/zabbix_agentd.conf -O /etc/zabbix/zabbix_agentd.conf
-wget https://raw.githubusercontent.com/kernelpanic0007/hiveOSScripts/main/customminer.sh -O /home/user/customminer.sh
-wget https://raw.githubusercontent.com/kernelpanic0007/hiveOSScripts/main/rig.conf -O /hive-config/rig.conf
+wget https://raw.githubusercontent.com/kernelpanic0007/dnar/main/amd-oc.conf -O /hive-config/amd-oc.conf
+wget https://raw.githubusercontent.com/kernelpanic0007/dnar/main/zabbix_agentd.conf -O /etc/zabbix/zabbix_agentd.conf
+wget https://raw.githubusercontent.com/kernelpanic0007/dnar/main/customminer.sh -O /home/user/customminer.sh
+wget https://raw.githubusercontent.com/kernelpanic0007/dnar/main/rig.conf -O /hive-config/rig.conf
 #start zabbix
 systemctl start zabbix-agent 
 systemctl enable zabbix-agent 
 
 #make scripts executable
 chmod +x /home/user/customminer.sh
-chmod +x /home/user/gputemp.sh
 
 #replace dummy var with wallet
 
@@ -50,7 +49,7 @@ sed -i 's/nodeipaddressvar/TYPEINYOURNODEIP/g' /home/user/customminer.sh
 sed -i 's/nodeipport/TYPEINYOURNODEPORT/g' /home/user/customminer.sh
 
 #replace zabbix HostName with systems
-sed -i 's/zabbixipaddressvar/TYPEINYOURNODEIPg' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/zabbixipaddressvar/TYPEINYOURZABBIXSERVERIP/g' /etc/zabbix/zabbix_agentd.conf
 sed -i 's/hostnamevar/'"$HOSTNAME"'/g' /etc/zabbix/zabbix_agentd.conf
 
 #zabbix log file
