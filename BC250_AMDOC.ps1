e9cab884536c8842f1f9320b8c7174178c60aed7
kernelpanic007 <josalamanca007@gmail.com>
2024-02-16 14:08:29 -0600

    Add files via upload

---
+++ BC250_AMDOC.ps1
@@ -0,0 +1,40 @@
+﻿Clear-Host
+$AMDOC = @(
+    [PSCustomObject]@{
+        ComputerName = "R03BC250-01"
+        CoreClock = 1601
+        CoreVoltage = 801
+    }
+    ,
+    [PSCustomObject]@{
+        ComputerName = "R03BC250-02"
+        CoreClock = 1602
+        CoreVoltage = 802
+    }
+)
+
+$creds = (Get-Credential user)
+
+Foreach ($oc in $AMDOC)
+{
+    # connect to rig 
+    $session = New-SSHSession -ComputerName $oc.ComputerName -Credential $creds –AcceptKey
+    $stream = New-SSHShellStream -SSHSession $session -TerminalName dumb
+
+    # drop to the BASH shell
+    $stream.WriteLine('shell')
+    $stream.Read()
+    sleep 1
+    $cmd = ('sed -i "s/CORE_CLOCK=.*/CORE_CLOCK=\"' + $oc.CoreClock + '\"/g" /hive-config/amd-oc.conf && sed -i "s/CORE_VDDC=.*/CORE_VDDC=\"' + $oc.CoreVoltage + '\"/g" /hive-config/amd-oc.conf')
+    Write-Host $cmd
+    $stream.WriteLine($cmd)
+    $stream.Read()
+    sleep 3
+    $stream.WriteLine('/hive/sbin/amd-oc')
+    $stream.Read()
+    sleep 3
+    # close the SSH session and stream
+    $stream.Close()
+    # close session
+    Remove-SSHSession -SSHSession $session | Out-Null
+}
---
+++ UpdateJanusMinerDnarBC250.ps1
@@ -0,0 +1,36 @@
+﻿Clear-Host
+$creds = (Get-Credential user)
+#Host name, make sure you're able to nslookup the hostname 
+$BC250miners = @('R01BC250','R02BC250','R03BC250')
+Foreach ($rig in $BC250miners)
+{
+    For ($i=1; $i -le 12; $i++) {
+
+    $hostid = $i.ToString("00")
+    $computername = "$rig-$hostid"
+    # connect to rig 
+    $session = New-SSHSession -ComputerName $computername -Credential $creds –AcceptKey
+    $stream = New-SSHShellStream -SSHSession $session -TerminalName dumb
+
+    # drop to the BASH shell
+    $stream.WriteLine('shell')
+    $stream.Read()
+    sleep 1
+    $stream.WriteLine('wget -4 https://raw.githubusercontent.com/kernelpanic0007/dnar/main/customminer.sh -O /home/user/customminer.sh')
+    $stream.Read()
+    sleep 3
+    $stream.WriteLine('chmod +x /home/user/customminer.sh')
+    $stream.Read()
+    sleep 3
+    $stream.WriteLine("sed -i 's/walletaddress/WALLETADDRESS/g' /home/user/customminer.sh && sed -i 's/nodeipaddressvar/NODEIPADDRESS/g' /home/user/customminer.sh && sed -i 's/nodeipport/NODEPORT/g' /home/user/customminer.sh && /home/user/customminer.sh")
+    $stream.Read()
+    sleep 3
+
+    # close the SSH session and stream
+    $stream.Close()
+    # close session
+    Remove-SSHSession -SSHSession $session | Out-Null
+
+    }
+
+}
